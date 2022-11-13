c19fdfa1852b239cbe55628e8d2429e4ba569110
Sofia92 <sofiasosaperez@outlook.com>
2022-11-13 13:41:08 -0600

    fixed bug in css style sheet

--- game.js
+++ game.js
@@ -1,22 +1,107 @@
 // this is where i am going to pull the quiz questions from to display on web. 
+const question = document.querySelector('#question');
+const choices = Array.from(document.querySelectorAll('.choice-text'));
+const progressText = document.querySelector('#progressText');
+const scoreText = document.querySelector('#score');
 
-// let questions = [
-//     {
-//     Number: 1,
-//     question: "What does HTML stand for?",
-//     answer: "Hyper Text Markup Language",
-//     options: [
-//       "Hyper Text Preprocessor",
-//       "Hyper Text Markup Language",
-//       "Hyper Terminal Multiple Language",
-//       "Hyper Tool Multi Language"
-//     ]
-//   },
+let currentQuestion = {}
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
+let availableQuestions = []
 
-//     {
-//     Number: 2,
-//     question: ""
-//     answer: ""
-//     options: [""
-//     ]
-//   },
+let question = [
+    { //question one 
+        question: "what is bla bla bla",
+        choice1: '2',
+        choice2: '4',
+        choice3: '6',
+        choice4: '5',
+        asnwer: 2,
+    },
+    {
+        question: "who is bla bla bla",
+        choice1: '2',
+        choice2: '4',
+        choice3: '6',
+        choice4: '5',
+        asnwer: 4,
+    },
+    {
+        question: "where is bla bla bla",
+        choice1: '2',
+        choice2: '4',
+        choice3: '6',
+        choice4: '5',
+        asnwer: 6,
+    },
+    {
+        question: "When is bla bla bla",
+        choice1: '2',
+        choice2: '4',
+        choice3: '6',
+        choice4: '5',
+        asnwer: 5,
+    },
+]
+
+const SCORE_POINTS = 100
+const MAX_QUESTIONS = 4
+
+startGame = () => {
+    // questionCounter = 0
+    score = 0
+    availableQuestions = [...questions]
+    getNewQuestion()
+}
+
+getNewQuestion = () => {
+    if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
+
+        return window.location.assign('/end.html')
+    }
+
+    questionCounter++
+    progressText.innerText = 'Question ${questionCounter} of ${MAX_QUESTIONS}'
+
+    const quesntionIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[quesntionIndez]
+    question.innerText = currentQuestion.question
+
+    choices.forEach(choice => {
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion['choice' + number]
+    })
+
+    availableQuestions.splice(quesntionIndex, 1)
+
+    acceptingAnswers = true
+}
+
+choices.forEach(choice => {
+    choice.addEventListener('click' e => {
+        if (!acceptingAnswers) return
+        acceptingAnswers = false
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset['number']
+
+        let classToApply === selectedAnswer == currentQuestion.asnwer ? 'correct' :
+            'incorrect'
+
+        if (classToApply === 'correct') {
+            incrementScore(SCORE_POINTS)
+        }
+        selectedChoice.parentElement.classList.add(classToApply)
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply)
+            getNewQuestion()
+
+        }, 1000)
+    })
+})
+incrementScore = num => {
+    score +=num
+    scoreText.innerText = score
+}
